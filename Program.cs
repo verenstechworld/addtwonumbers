1a7044ed68b35dc3938bf637863c3b7ce46ef9ee
verenstechworld <kenolyoveren@gmail.com>
2024-10-06 00:31:35 +0200

    addedtwonumbers

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int number1, number2, sum; // variable declaration
+
+Console.Write("Please enter first number:");
+number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter second number:");
+number2 = Convert.ToInt32(Console.ReadLine());
+
+sum = number1 + number2;
+Console.WriteLine("the sum: {0}" + sum);
+Console.ReadKey();
