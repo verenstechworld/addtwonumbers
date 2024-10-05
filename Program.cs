eb66b429284639ba75608e7d0a37477eda2ce099
verenstechworld <kenolyoveren@gmail.com>
2024-10-06 01:17:50 +0200

    added two numbers

--- Program.cs
+++ Program.cs
@@ -9,5 +9,7 @@ Console.Write("Please enter second number:");
 number2 = Convert.ToInt32(Console.ReadLine());
 
 sum = number1 + number2;
-Console.WriteLine("the sum: {0}" + sum);
+Console.WriteLine("the sum: {0}", sum);
+Console.WriteLine("Please press any key to close");
+
 Console.ReadKey();
